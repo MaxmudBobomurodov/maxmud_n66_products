4cb83e6722594c3e01b07d1967597876a283ba2e
Maxmud <maxmudbobomurodov151@gmail.com>
2025-05-27 01:02:17 +0500

    creating functions and menus

---
+++ product_menu.py
@@ -0,0 +1,23 @@
+def menu():
+    print("""
+    1.show products
+    2.add products
+    3.delete products
+    4.change status
+    5.exit
+    """)
+    choice = input("enter your choice:")
+    if choice == '1':
+        pass
+    elif choice == '2':
+        pass
+    elif choice == '3':
+        pass
+    elif choice == '4':
+        pass
+    elif choice == '5':
+        pass
+    else:
+        print("invalid choice")
+        menu()
+        
